4283e52784f018027cbba9385d37e5287c94c57f
GiSeong Lee <cj8928@gmail.com>
2024-02-06 18:10:49 +0900

    헤더 링크 스타일 추가

--- src/styles/Header.ts
+++ src/styles/Header.ts
@@ -5,7 +5,7 @@ export const HeaderContainer = styled.header`
   color: black;
   padding: 40px 20px;
   display: flex;
-  justify-content: space-between;
+  align-items: center;
 `;
 
 export const HeaderTitle = styled.h3`
@@ -15,10 +15,16 @@ export const HeaderTitle = styled.h3`
   cursor: pointer;
 `;
 
+export const HeaderLink = styled.h4`
+  cursor: pointer;
+  margin-left: 24px;
+`;
+
 export const SearchContainer = styled.div`
   display: flex;
   width: 220px;
   align-items: center;
+  margin-left: auto;
 `;
 
 export const SearchInputField = styled.input`
