6d841cef1b7c187c7e37a4eb2d305a7fcb08bee8
GiSeong Lee <cj8928@gmail.com>
2024-02-06 18:10:37 +0900

    홈 세컨드 캐러셀 스타일 추가

--- src/styles/Carousel.ts
+++ src/styles/Carousel.ts
@@ -5,21 +5,39 @@ import styled from "styled-components";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export const NextSlideBtn = styled.div`
+export const SlideArrow = styled.div`
   display: block;
+  background-color: black;
   color: white;
   width: 40px;
   height: 40px;
-`;
-export const PrevSlideBtn = styled.div`
-  display: block;
-  color: white;
-  width: 40px;
-  height: 40px;
+  z-index: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  border-radius: 50%;
+  opacity: 0.7;
+  &.left {
+    left: 34px;
+  }
+  &.right {
+    right: 34px;
+  }
+
+  .secondary-slide-container &.left {
+    transform: translateY(-130%);
+  }
+  .secondary-slide-container &.right {
+    transform: translateY(-130%);
+  }
 `;
 
 export const HomeMainSlider = styled(Slider)`
-  width: 100%; /* 원하는 너비 설정 */
+  width: 100%;
   height: 550px;
   margin: 0 auto;
   border-radius: 1rem;
@@ -59,5 +77,51 @@ export const HomeMainSlideTitleLink = styled.a`
   text-decoration: none;
 `;
 
-export const HomeSecondarySlider = styled(Slider)``;
-export const HomeSecondarySlide = styled.div``;
+export const HomeSecondarySlideTitle = styled.h3`
+  width: 100%;
+  padding-left: 20px;
+  margin-top: 50px;
+  cursor: pointer;
+`;
+
+export const HomeSecondarySlider = styled(Slider)`
+  width: 100%;
+  height: 300px;
+  margin: 0 auto;
+  border-radius: 1rem;
+  .slick-track {
+    max-height: 300px;
+    display: flex;
+    justify-content: center;
+  }
+  .slick-list {
+    border-radius: 1rem;
+    max-height: 300px;
+  }
+  .slick-slide {
+    margin: 0 5px;
+  }
+`;
+export const HomeSecondarySlide = styled.div`
+  color: white;
+  border: 1px solid white;
+  border-radius: 1rem;
+  background-color: white; /* 원하는 배경색 설정 */
+  height: 270px;
+`;
+
+export const HomeSecondarySlideWebtoonTitleBox = styled.div`
+  color: black;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  padding-left: 12px;
+  text-decoration: none;
+`;
+
+export const HomeSecondarySlideTitleLink = styled.a`
+  font-size: 13px;
+  color: inherit;
+  text-decoration: none;
+  word-wrap: break-word;
+`;
