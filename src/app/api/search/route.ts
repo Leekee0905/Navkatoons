eb223a2287447e01c819ebe21962c1d951b9f31e
GiSeong Lee <cj8928@gmail.com>
2024-07-09 17:33:45 +0900

    try catch문 추가

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -6,13 +6,17 @@ import { apiInstance } from "../apiInstance";
 export const GET = async (req: NextRequest, res: NextResponse) => {
   const params = req.nextUrl.searchParams;
   const keyword = params.get("key");
-  const searchRes = await apiInstance.get("/webtoons", {
-    params: {
-      keyword: keyword,
-    },
-  });
 
-  return NextResponse.json({
-    response: searchRes.data.webtoons,
-  });
+  try {
+    const searchRes = await apiInstance.get("/webtoons", {
+      params: {
+        keyword: keyword,
+      },
+    });
+    return NextResponse.json({
+      response: searchRes.data.webtoons,
+    });
+  } catch (error) {
+    return NextResponse.json({ error: "" }, { status: 404 });
+  }
 };
