c64d24771be717c3b6e69e7b4e4059fe41e7fae1
GiSeong Lee <cj8928@gmail.com>
2024-07-10 18:05:45 +0900

    이미지 url format추가

--- src/app/api/mainCarousel/route.ts
+++ src/app/api/mainCarousel/route.ts
@@ -21,7 +21,7 @@ export const GET = async (req: NextRequest, res: NextResponse) => {
         perPage: 15,
       },
     });
-    let kakaoPage = await apiInstance.get("/webtoons", {
+    const kakaoPage = await apiInstance.get("/webtoons", {
       params: {
         provider: "KAKAO_PAGE",
         updateDay: day,
@@ -31,16 +31,25 @@ export const GET = async (req: NextRequest, res: NextResponse) => {
 
     switch (service) {
       case "naver": {
+        naver.data.webtoons.forEach((e): any => {
+          e.thumbnail[0] = `${e.thumbnail[0]}?format=webp`;
+        });
         return NextResponse.json({
           response: naver.data.webtoons,
         });
       }
       case "kakao": {
+        kakao.data.webtoons.forEach((e): any => {
+          e.thumbnail[0] = `${e.thumbnail[0]}?format=webp`;
+        });
         return NextResponse.json({
           response: kakao.data.webtoons,
         });
       }
       case "kakaoPage": {
+        kakaoPage.data.webtoons.forEach((e): any => {
+          e.thumbnail[0] = `${e.thumbnail[0]}?format=webp`;
+        });
         return NextResponse.json({
           response: kakaoPage.data.webtoons,
         });
